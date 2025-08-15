cc9dab168d2383bff9015d7e2c5c672742bfe23a
NoahRaffensparger <noahtraff@gmail.com>
2025-08-14 20:03:51 -0500

    connected formspree

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,10 @@ export default function InquiryForm() {
         <>
             <div className="bg-gray-300 pt-12 pb-8 px-4 xl:h-[800px] xl:flex xl:flex-col xl:place-content-center">
                 <h1 className="text-center text-4xl font-semibold pb-4 text-black/80">let's talk!</h1>
-                <form className="mx-4 bg-white rounded-xl p-4 lg:w-3/5 mx-auto">
+                <form
+                    action="https://formspree.io/f/xzzvwjeo"
+                    method="POST"
+                    className="mx-4 bg-white rounded-xl p-4 lg:w-3/5 mx-auto">
                     <input className="bg-gray-200 border-1 border-gray-300 px-4 py-3 mb-6 w-full" type="text" id="name" name="name" required placeholder="Name" />
                     <input className="bg-gray-200 border-1 border-gray-300 px-4 py-3 mb-6 w-full" type="email" id="email" name="email" required placeholder="Email" />
                     <input className="bg-gray-200 border-1 border-gray-300 px-4 py-3 mb-6 w-full" type="tel" id="phone" name="phone" placeholder="Phone" />
